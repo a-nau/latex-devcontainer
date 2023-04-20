2b8954daf6ad8463aabfaa5ee4372a7c959642d6
Alex Naumann <alexander@naumail.de>
2023-04-20 17:30:19 +0200

    fix: fix bug in folder name

--- misc/copy_settings.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Copy VS Code Settings
-mkdir -p .vscode
-cp -n .devcontainer/settings.json .vscode
--- misc/initial_setup.sh
+++ misc/initial_setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copy VS Code Settings
+mkdir -p .vscode
+cp -n .devcontainer/settings.json .vscode
 # Copy Github CI
 mkdir -p .github/workflows
 cp -n .devcontainer/ci/latex_build.yml .github/workflows
